d06ed8b624ada6526a95f6ae8846785fa45ac0e2
AlexAlves5000 <alexalves5000@gmail.com>
2022-11-22 12:54:42 -0300

    Layout Cadastro de Feriado - ok

--- src/pages/feriados/Form.vue
+++ src/pages/feriados/Form.vue
@@ -1,21 +1,60 @@
 <template>
   <q-page padding>
-    <div class="container">
-      <div class="row col-12 bg-secondary justify-center">
-        <p class="text-h5">Cadastro de Feriados</p>
-      </div>
-      <div class="bg-grey-2 q-pa-sm rounded-borders">
-        Feriado/Dia não útil aplicável(is) em qual(is) sistema(s):
+    <p class="text-h5 text-center">Registro de Feriados</p>
+
+    <q-form class="" @submit.prevent="handleFeriado">
+      <div class="q-pa-md">
+        <label>Feriado aplicável em qual(is) sistema(s):</label>
         <q-option-group
           name="accepted_genres"
-          v-model="accepted"
+          v-model="form.sistema"
           :options="options"
           type="checkbox"
-          color="primary"
+          color="info"
           inline
+          class="col-12"
         />
       </div>
-    </div>
+
+      <div class="row">
+        <div class="q-pa-md col-12 col-sm-3 col-md-3">
+          <label>Data do Feriado</label>
+          <q-input type="date" v-model="form.data" outlined> </q-input>
+        </div>
+
+        <div class="col-12 q-pa-md">
+          <label>descrição do Feriado</label>
+          <q-input
+            outlined
+            label="Feriado"
+            v-model="form.feriado"
+            lazy-rules
+            class="col-12"
+            :rules="[
+              (val) =>
+                (val && val.length > 0) || 'Digite a descrição do feriado',
+            ]"
+          />
+        </div>
+      </div>
+
+      <div class="row justify-center q-gutter-md">
+        <q-btn
+          label="Registrar"
+          color="primary"
+          outlined
+          size="md"
+          type="submit"
+        />
+        <q-btn
+          label="Voltar"
+          color="secondary"
+          size="md"
+          outlined
+          :to="{ name: 'diaferiado' }"
+        />
+      </div>
+    </q-form>
   </q-page>
 </template>
 
@@ -26,9 +65,14 @@ import { defineComponent, ref, onMounted } from "vue";
 
 export default defineComponent({
   name: "PageAdicionarFeriado",
-
   setup() {
-    const submitResult = ref([]);
+    const form = ref({
+      sistema: ["ejud", "pje"],
+      data: "",
+      feriado: "",
+    });
+
+    // const submitResult = ref([]);
 
     // const feriados = ref([]);
     // const loading = ref(true);
@@ -37,6 +81,8 @@ export default defineComponent({
     // const pagination = ref({
     //   rowsPerPage: 10,
     // });
+
+    const handleFeriado = "";
 
     // const handleListFeriados = async () => {
     //   try {
@@ -53,8 +99,9 @@ export default defineComponent({
     // });
 
     return {
+      form,
+      handleFeriado,
       accepted: ref(["ejud", "pje"]),
-      submitResult,
 
       options: [
         {
